25832a79392f7aee5897304ce0c00fb7ebc5c169
macbf <macbf0@gmail.com>
2022-02-12 15:20:36 -0300

    Criacao models blog

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,27 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    
+    Status_Choice = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado'),
+    )
+
+    title = models.CharField(max_length=300)
+    label = models.SlugField(max_length=300, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    text = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=Status_Choice, default='rascunho')
+
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
