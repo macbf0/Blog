043c1d2cc4840a26834d66ad56d8fa8d0b596945
macbf <macbf0@gmail.com>
2022-02-13 08:53:27 -0300

    Personalizando admin Post

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -3,4 +3,10 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','label')
+    list_display = ('title','label','author','text','publish','status')
+    list_filter = ('author','status','publish','date_created')
+    search_fields = ('title','text')
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'label':('title',)}
+    raw_id_fields = ('author',)
+    ordering = ('status','publish')
