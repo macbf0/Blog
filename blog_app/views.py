70fefe46be95911b6c92020f8fccd202d1246dd6
macbf <macbf0@gmail.com>
2022-02-14 12:19:48 -0300

    Criacao de views list e detail

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.publisehd.all()
+    return render(request, 'blog_app/post/list.html',{'posts': posts})
+
+
+def post_detail(request, post, day, month, year):
+    post = get_object_or_404(Post, label=post,status='publicado',publish_year=year, publish_month=month, publish_day=day )
+    return render(request, 'blog_app/post/detail.html', {'post':post})
